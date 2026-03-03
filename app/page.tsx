57fa27acf5e5cdc0049d8b93eba89b20a0706f46
rrealliee <brilliani.jeshia2812@gmail.com>
2026-03-03 08:11:49 +0800

    Update page.tsx (redirect localhost base to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-"use client"
-import { useEffect, useState } from "react"
-import { supabase } from "../lib/supabaseClient"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  const [pegawai, setPegawai] = useState<any[]>([])
-
-  useEffect(() => {
-    getPegawai()
-  }, [])
-
-  async function getPegawai() {
-    const { data, error } = await supabase
-      .from("pegawai")
-      .select("*")
-
-    if (error) console.log(error)
-    else setPegawai(data)
-  }
-
-return (
-  <div style={{ padding: 40 }}>
-    <h3>{process.env.NEXT_PUBLIC_SUPABASE_URL}</h3>
-    <h1>ORBIT - Data Pegawai</h1>
-
-    {pegawai.map((p) => (
-      <div key={p.id}>
-        • {p.nama}
-      </div>
-    ))}
-  </div>
-)}
+  redirect("/login")
+}
